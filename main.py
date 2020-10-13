18198cc5aa44f9bb98cc060c3668463bb017dad6
Joe Paterson <iamtempme@gmail.com>
2020-10-13 18:53:36 +0100

    added prelim and tested folder creation + 7zip download/ install

--- main.py
+++ main.py
@@ -1,31 +1,73 @@
 import config as cfg
+import os
+import wget 
+from datetime import date as dt
+
+# Move files to recycling bin and clean this according to setting in config
 
 
-def storeData(): #{ 
+# ================
+# Global Variables 
+# ================
+PWD = 'C:\\PyWindowsCleaner\\'
+DATE = dt.today()
 
-#}
+def prelim():
+    print("Prelim started")
+    dirs = os.listdir('C:\\')
+    count = 0
+    found = False
+    while count < len(dirs) and found != True:
+        if dirs[count] == 'PyWindowsCleaner':
+            found = True
+        count += 1
+
+    if found != True: 
+        print("Root folder not found. Creating")
+        os.makedirs(PWD)
+    sevenzip()
 
 
-def backup(): # { 
+def sevenzip():  
+    print("Checking if 7-Zip exist")
+    dirs = os.listdir('C:\\Program Files (x86)\\')
+    dirs += os.listdir('C:\\Program Files\\')
+    found = False
+    count = 0
+    while count < len(dirs) and found != True:
+        count += 1
+        if dirs[count - 1] == '7-Zip': 
+            print("7-Zip found")
+            found = True
+    if found != True: 
+        print("7-Zip not found. Installing")
+        wget.download('https://www.7-zip.org/a/7z1900.exe', PWD + "7-ZipSetup.exe")
+        os.system('cmd /c ' + PWD + '7-ZipSetup.exe /S && del ' + PWD + '7-ZipSetup.exe')
+ 
 
-#}
 
-def zip(): # { 
-
-#}
-
-def clean(): # { 
-
-#}
+def backup():  
+    pass
 
 
-def main(): # { 
+def zip():  
+    pass
+
+
+def clean():  
+    pass
+
+
+
+
+def main():  
     # Check if this is first time running
-    first_run = cfg.main();
+    first_run = cfg.main()
     if first_run == 1: 
-        exit();
+        exit()
     else: 
         jsonData = first_run
+    prelim()
 
     # Back upper
         # Zipper
@@ -33,7 +75,7 @@ def main(): # {
 
 
 
-#}
+
       
 
 

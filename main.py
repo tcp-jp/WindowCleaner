a863d56de0e77d262ebb154917b3a35df437bcd7
Joe Paterson <iamtempme@gmail.com>
2020-10-13 19:43:58 +0100

    working on backup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import config as cfg
+import json
 import os
 import wget 
 from datetime import date as dt
@@ -24,47 +25,49 @@ def prelim():
         os.makedirs(PWD)
     sevenzip()
 
-
-def sevenzip():
-    print("Checking if 7-Zip exist")
-    dirs = os.listdir('C:\\Program Files (x86)\\')
-    dirs += os.listdir('C:\\Program Files\\')
+def checkFolderExists(folderName, path):
+    dirs=os.listdir(path)
     found = False
     count = 0
     while count < len(dirs) and found != True:
         count += 1
-        if dirs[count - 1] == '7-Zip':
-            print("7-Zip found")
-            found = True
-    if found != True:
-        print("7-Zip not found. Installing")
-        wget.download('https://www.7-zip.org/a/7z1900.exe', PWD + "7-ZipSetup.exe")
-        os.system('cmd /c ' + PWD + '7-ZipSetup.exe /S && del ' + PWD + '7-ZipSetup.exe')
+        if dirs[count - 1] == folderName:
+            print(folderName + "found")
+            return True
+    return False
+    
 
+def sevenzip():
+    print("Checking if 7-Zip exist")
+    if checkFolderExists("7-Zip", 'C:\\Program Files (x86)\\') != True:
+        if checkFolderExists("7-Zip", 'C:\\Program Files\\') != True:
+            print("7-Zip not found. Installing")
+            wget.download('https://www.7-zip.org/a/7z1900.exe', PWD + "7-ZipSetup.exe")
+            os.system('cmd /c ' + PWD + '7-ZipSetup.exe /S && del ' + PWD + '7-ZipSetup.exe')
+            if checkFolderExists("7-Zip"< 'C:\\Program Files (x86)\\') != True:
+                if checkFolderExists("7-Zip"< 'C:\\Program Files\\') != True:
+                    print("7-Zip failed to install")
+                else:
+                    os.system('cmd /c set path=%path%;c:\program files\7-zip;')
+            else:
+                os.system('cmd /c set path=%path%;c:\program files (x86)\7-zip;')
 
+def backup(data):
+    print(data)
+    backupFolders=data["Backup"]["BackupFolders"]
+    zipPath=data["Backup"]["ZipTo"]
+    for folder in backupFolders:
+      break
+      #os.system('
+    # for each folder in backupFolders add the folder to the archive
+    makeZip(zipPath)
 
-def backup():
+def makeZip(path): 
+    # 7zip archive
     pass
 
-def backup(): 
+def clean(data): 
     pass
-
-
-def zip(): 
-    pass
-
-
-def clean(): 
-    pass
-
-
-def zip():
-    pass
-
-
-def clean(): 
-    pass
-
 
 def main():
     # Check if this is first time running
@@ -72,9 +75,11 @@ def main():
     if first_run == 1:
         exit()
     else:
-        jsonData = first_run
+        jsonData = cfg.readConfig()
     prelim()
+    backup(jsonData)
+    clean(jsonData)
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
